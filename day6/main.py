306f6f48d4e1e7816c47d25d7cf062f6451fe608
Mike Dougherty <mike.dougherty@missionlane.com>
2021-12-09 15:29:27 -0800

    day 6 part 1

--- day6/main.py
+++ day6/main.py
@@ -1,8 +1,22 @@
 import sys
 
+NUM_DAYS = 80
+
 
 def main(input_file):
-    pass
+    fishs = list(map(int, input_file.read().split(",")))
+
+    print(f"initial: fish={len(fishs)}")
+    for day in range(1, NUM_DAYS + 1):
+        new_fish = 0
+        for i in range(len(fishs)):
+            fishs[i] -= 1
+            if fishs[i] < 0:
+                fishs[i] = 6
+                new_fish += 1
+
+        fishs.extend([8] * new_fish)
+        print(f"{day=} fish={len(fishs)}")
 
 
 if __name__ == "__main__":
