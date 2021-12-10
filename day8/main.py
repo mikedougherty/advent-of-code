5daf81be1f5be050ec5e3bf3f6c49a8b283fe6c3
Mike Dougherty <mike.dougherty@missionlane.com>
2021-12-09 17:02:56 -0800

    day 8 part 1

--- day8/main.py
+++ day8/main.py
@@ -1,8 +1,35 @@
 import sys
 
+DIGITS = {
+    0: "abcefg",
+    1: "cf",
+    2: "acdeg",
+    3: "acdfg",
+    4: "bcdf",
+    5: "abdfg",
+    6: "abdefg",
+    7: "acf",
+    8: "abcdefg",
+    9: "abcdfg",
+}
+
+TARGETS = [1, 4, 7, 8]
+
 
 def main(input_file):
-    pass
+    entries = [
+        tuple(x.split() for x in line.split(" | ", 1))
+        for line in input_file.readlines()
+    ]
+
+    hits = 0
+    for entry in entries:
+        for target in TARGETS:
+            for output in entry[1]:
+                if len(output) == len(DIGITS[target]):
+                    hits += 1
+
+    print(f"Detected {hits} instances of {TARGETS}")
 
 
 if __name__ == "__main__":
