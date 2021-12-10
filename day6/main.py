8580461fecb9475b495bc0f8da175dd508829de2
Mike Dougherty <mike.dougherty@missionlane.com>
2021-12-09 16:13:01 -0800

    day 6 part 2

--- day6/main.py
+++ day6/main.py
@@ -1,22 +1,22 @@
 import sys
 
-NUM_DAYS = 80
+NUM_DAYS = 256
+REPRO_AGE = 2
+GEST_PERIOD = 7
 
 
 def main(input_file):
-    fishs = list(map(int, input_file.read().split(",")))
+    fish_phases = [0] * (REPRO_AGE + GEST_PERIOD)
+    for fish in list(int(x) for x in input_file.read().split(",")):
+        fish_phases[REPRO_AGE + (GEST_PERIOD - fish) - 1] += 1
 
-    print(f"initial: fish={len(fishs)}")
+    print(f"initial: fish={sum(fish_phases)}, {fish_phases=}")
     for day in range(1, NUM_DAYS + 1):
-        new_fish = 0
-        for i in range(len(fishs)):
-            fishs[i] -= 1
-            if fishs[i] < 0:
-                fishs[i] = 6
-                new_fish += 1
+        new_fish = fish_phases.pop()
+        fish_phases.insert(0, new_fish)
+        fish_phases[REPRO_AGE] += new_fish
 
-        fishs.extend([8] * new_fish)
-        print(f"{day=} fish={len(fishs)}")
+        print(f"{day=} fish={sum(fish_phases)}, {fish_phases=}")
 
 
 if __name__ == "__main__":
