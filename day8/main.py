161f2dd216cd6ea1590bb81953c49e9cfb798dc4
Mike Dougherty <mike.dougherty@missionlane.com>
2021-12-09 19:00:23 -0800

    day 8 part 2
    this one sucked hard :/ not sure what the right approach is, because this doesn't seem like it

--- day8/main.py
+++ day8/main.py
@@ -1,19 +1,77 @@
 import sys
+import collections
 
 DIGITS = {
-    0: "abcefg",
-    1: "cf",
+    # Sorted by length of value
+    1: "cf",  # Unique
+    7: "acf",  # Unique
+    4: "bcdf",  # Unique
     2: "acdeg",
     3: "acdfg",
-    4: "bcdf",
     5: "abdfg",
+    0: "abcefg",
     6: "abdefg",
-    7: "acf",
-    8: "abcdefg",
     9: "abcdfg",
+    8: "abcdefg",  # Unique
 }
 
-TARGETS = [1, 4, 7, 8]
+
+UNIQUES = (1, 4, 7, 8)
+
+
+def create_decipher_table(ciphertext):
+    ciphertext = set(ciphertext)
+    digit_table = {}
+
+    for u in UNIQUES:
+        for cipher_digit in list(ciphertext):
+            if len(DIGITS[u]) == len(cipher_digit):
+                # print(f"found {u}")
+                digit_table[u] = cipher_digit
+                ciphertext.remove(cipher_digit)
+
+    for cipher_digit in list(ciphertext):
+        if len(cipher_digit) != len(DIGITS[3]):
+            continue
+
+        if (set(digit_table[1]) | set(cipher_digit)) == set(cipher_digit):
+            # print(f"found 3 {cipher_digit=}")
+            digit_table[3] = cipher_digit
+            ciphertext.remove(cipher_digit)
+        elif len(set(digit_table[4]) - set(cipher_digit)) == 2:
+            # print(f"found 2 {cipher_digit=}")
+            digit_table[2] = cipher_digit
+            ciphertext.remove(cipher_digit)
+        else:
+            # print(f"found 5 {cipher_digit=}")
+            digit_table[5] = cipher_digit
+            ciphertext.remove(cipher_digit)
+
+    for cipher_digit in list(ciphertext):
+        if len(cipher_digit) != len(DIGITS[0]):
+            continue
+        if len(set(cipher_digit) - set(digit_table[5])) == 2:
+            # print(f"found 0 {cipher_digit=}")
+            digit_table[0] = cipher_digit
+            ciphertext.remove(cipher_digit)
+
+    for cipher_digit in list(ciphertext):
+        if len(set(cipher_digit) - set(digit_table[4])) == 3:
+            # print(f"found 6 {cipher_digit=}")
+            digit_table[6] = cipher_digit
+            ciphertext.remove(cipher_digit)
+
+    assert len(ciphertext) == 1
+    digit_table[9] = ciphertext.pop()
+
+    return dict(("".join(sorted(v)), k) for k, v in digit_table.items())
+
+
+def decipher(input, output):
+    table = create_decipher_table(input)
+    return tuple(table["".join(sorted(i))] for i in input), tuple(
+        table["".join(sorted(o))] for o in output
+    )
 
 
 def main(input_file):
@@ -23,13 +81,20 @@ def main(input_file):
     ]
 
     hits = 0
-    for entry in entries:
-        for target in TARGETS:
-            for output in entry[1]:
-                if len(output) == len(DIGITS[target]):
+    result = 0
+    for input, output in entries:
+        plain_input, plain_output = decipher(input, output)
+        print(f"{input} | {output} -> {plain_input} | {plain_output}")
+
+        for target in UNIQUES:
+            for digit in plain_output:
+                if digit == target:
                     hits += 1
 
-    print(f"Detected {hits} instances of {TARGETS}")
+        result += int("".join(map(str, plain_output)))
+
+    print(f"Detected {hits} instances of {UNIQUES}")
+    print(f"Sum of all outputs: {result}")
 
 
 if __name__ == "__main__":
