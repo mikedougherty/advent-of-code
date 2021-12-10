c0a38d2736ae38eb4815f38bb58ce8bd670ef005
Mike Dougherty <mike.dougherty@missionlane.com>
2021-12-09 16:27:46 -0800

    day 7 part 1

--- day7/main.py
+++ day7/main.py
@@ -1,8 +1,29 @@
 import sys
 
+FUEL = 1
+
+
+def cost_to_align(x, positions):
+    cost = 0
+    for p in positions:
+        distance = abs(x - p)
+        cost += FUEL * distance
+    return cost
+
 
 def main(input_file):
-    pass
+    positions = list(map(int, input_file.read().split(",")))
+    possibles = list(range(max(positions)))
+
+    answer, answer_cost = 0, sys.maxsize
+    for possible in possibles:
+        cost = cost_to_align(possible, positions)
+        print(f"Cost to align to {possible}: {cost}")
+        if cost < answer_cost:
+            answer = possible
+            answer_cost = cost
+
+    print(f"Align on position {answer} for cost {answer_cost}")
 
 
 if __name__ == "__main__":
