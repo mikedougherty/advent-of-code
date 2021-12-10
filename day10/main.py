231412b50e729c3f850f3ab7d08d8ee3c3cb97a5
Mike Dougherty <mike.dougherty@missionlane.com>
2021-12-10 11:18:06 -0800

    day 10 part 1

--- day10/main.py
+++ day10/main.py
@@ -1,8 +1,41 @@
 import sys
 
+score_table = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+    None: 0,
+}
+
+ch_pairs = {
+    "[": "]",
+    "{": "}",
+    "(": ")",
+    "<": ">",
+}
+
+
+def first_corrupt_character(line):
+    stack = []
+    for ch in line:
+        if ch in ch_pairs:
+            # opening a new block
+            stack.append(ch)
+        else:
+            # closing a block, must match last thing on our stack
+            if ch_pairs[stack[-1]] == ch:
+                stack.pop()
+            else:
+                return ch
+
 
 def main(input_file):
-    pass
+    score = 0
+    for line in input_file.readlines():
+        score += score_table.get(first_corrupt_character(line.strip()))
+
+    print(f"Syntax error score: {score}")
 
 
 if __name__ == "__main__":
